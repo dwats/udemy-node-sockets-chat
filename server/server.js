ca3031d8a7d26d23caa87db65edf581980d8a458
Eric Julius <ejulius86@gmail.com>
2017-09-04 17:48:02 -0500

    Greet new user and alert chat

--- server/server.js
+++ server/server.js
@@ -16,14 +16,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('User connected');
 
+  socket.emit('newMessage', { from: 'admin', text: 'Welcome to the chat!', created: new Date().getTime() });
+  socket.broadcast.emit('newMessage', { from: 'admin', text: 'A new user has joined the chat.', created: new Date().getTime() });
+
+
   socket.on('createMessage', (message) => {
     const createMessage = _.pick(message, ['from', 'text']);
     createMessage.created = new Date().getTime();
     io.emit('newMessage', createMessage);
+    // socket.broadcast.emit('newMessage', createMessage);
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
+    socket.broadcast.emit('newMessage', { from: 'admin', text: 'A user has left the chat.', created: new Date().getTime() });
   });
 });
 
